66a8544094d114d40a28501e23c514161a50dad6
Justyna Sikora <110971323+JOSikora@users.noreply.github.com>
2024-11-14 18:18:25 +0100

    Add files via upload

---
+++ obliczenia.py
@@ -0,0 +1,13 @@
+import math
+
+print('Podaj liczbe a:')
+a = int(input())
+print('Podaj liczbe b:')
+b = int(input())
+print('a+b = ', a+b)
+print('a-b = ', a-b)
+print('a·b = ', a*b)
+print('a/b = ', a/b)
+print('pierwiastek z sumy = ', math.sqrt(a+b))
+print('a^b = ', a**b)
+print('b^a = ', b**a)
